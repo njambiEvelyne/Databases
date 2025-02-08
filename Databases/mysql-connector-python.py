f0c5381f0eff4b924f95fdea645e159b21908379
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-08 13:49:01 +0300

    Added files

--- Databases/mysql-connector-python.py
+++ Databases/mysql-connector-python.py
@@ -1,25 +1,28 @@
-import mysql.connector
+# import mysql.connector
+#
+# #Establish connection to MYSQL
+# mydb = mysql.connector.connect(
+#     host = "localhost",
+#     user = "root",
+#     password = "20Eve,lyne#76",
+#     port = 3306
+# )
+#
+# #Create the cursor
+# mycursor = mydb.cursor()
+#
+# #Execute the query
+# mycursor.execute("show databases")
+#
+# databases = mycursor.fetchall()
+#
+# #print the databases
+# for db in databases:
+#     print(db)
+#
+#
+# mycursor.close()
+# mydb.close()
 
-#Establish connection to MYSQL
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "20Eve,lyne#76",
-    port = 3306
-)
+print(6 | 3)
 
-#Create the cursor
-mycursor = mydb.cursor()
-
-#Execute the query
-mycursor.execute("show databases")
-
-databases = mycursor.fetchall()
-
-#print the databases
-for db in databases:
-    print(db)
-
-
-mycursor.close()
-mydb.close()
